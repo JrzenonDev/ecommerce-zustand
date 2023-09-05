cb4d5906e099f4a15016433e6d2cbec5a0a466b9
JrzenonDev <jrobertoonb@gmail.com>
2023-09-05 00:02:15 -0300

    feat: adding component Total in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { AvailableProducts } from "./components/AvailableProducts";
 import { Cart } from "./components/Cart";
+import { Total } from "./components/Total";
 
 function App() {
   return (
     <>
+      <Total />
       <AvailableProducts />
       <Cart />
     </>
